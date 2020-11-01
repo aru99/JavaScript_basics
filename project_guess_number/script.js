9fd0029fa3b30fddba9b6a3e7d025b681f2ce3d5
aru99 <mohammadarmanmavric@gmail.com>
2020-11-01 11:00:04 +0530

    Started working on the random number and comparision

--- project_guess_number/script.js
+++ project_guess_number/script.js
@@ -1,1 +1,20 @@
-'use strict';
+"use strict";
+
+console.log(document.querySelector(".message"));
+document.querySelector(".message").textContent = "🎉 Correct Number";
+console.log(document.querySelector(".message"));
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 10;
+
+document.querySelector(".guess").value = 23;
+console.log(document.querySelector(".guess").value);
+document.querySelector(".check").addEventListener("click", () => {
+  //   document.querySelector(".number").textContent = document.querySelector(
+  //     ".guess"
+  //   ).value;
+
+  //converting string into number for comparison with the random number
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+});
