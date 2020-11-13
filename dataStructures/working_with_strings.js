15659a953a63ce15f1ad1617c7ac3b7c6862b5a9
aru99 <mohammadarmanmavric@gmail.com>
2020-11-13 19:03:21 +0530

    done with booleans, splice and some other functions

--- dataStructures/working_with_strings.js
+++ dataStructures/working_with_strings.js
@@ -2,60 +2,85 @@
 
 const airline = "Tap airlines of luftanza";
 const plane = "A320";
-//--------------------------------------------------name right formatting start-------------------------------------
-// function that takes a passanger name and returns the right formatted passanger name
-const correctName = (passanger_name) => {
-  const lowerPassanger = passanger_name.toLowerCase();
-  const correctPassanger =
-    lowerPassanger[0].toUpperCase() +
-    lowerPassanger.slice(1, lowerPassanger.indexOf(" ")) +
-    " " +
-    lowerPassanger[lowerPassanger.indexOf(" ") + 1].toUpperCase() +
-    lowerPassanger.slice(lowerPassanger.indexOf(" ") + 2);
 
-  return correctPassanger;
-};
+//---------replacing start----------
+const priceUS = "4567,89$";
+//replace method takes two arguments, first the element to be replaced and the second with which it would be replaced
+//since the method retruns a string chaining is alowed
+const priceIN = priceUS.replace(",", ".").replace("$", "rs");
+console.log(priceIN);
+const announcement = "boarding at door no.3, Boording door no.3!! Asap😠 ";
+console.log(announcement.replace("door", "gate"));
+console.log(announcement.replaceAll("door", "gate"));
+//usinng regular expressions to do what replaceAll does
+console.log(announcement.replace(/door/g, "gate"));
+//---------replacing end----------
+
+//---------Boolean start----------
+const plane2 = "A320";
+console.log(plane2.includes("A320"));
+console.log(plane2.includes("Boeing"));
+//---------Boolean End----------
+
+//--------------------------------------------------name right formatting start-------------------------------------
+// // function that takes a passanger name and returns the right formatted passanger name
+// const correctName = (passanger_name) => {
+//   const lowerPassanger = passanger_name.toLowerCase();
+//   const correctPassanger =
+//     lowerPassanger[0].toUpperCase() +
+//     lowerPassanger.slice(1, lowerPassanger.indexOf(" ")) +
+//     " " +
+//     lowerPassanger[lowerPassanger.indexOf(" ") + 1].toUpperCase() +
+//     lowerPassanger.slice(lowerPassanger.indexOf(" ") + 2);
+
+//   return correctPassanger;
+// };
 
 //convering to lowercase and upper case, botht the methods does not require any arguments
-console.log(airline.toLowerCase());
-console.log(airline.toUpperCase());
+// console.log(airline.toLowerCase());
+// console.log(airline.toUpperCase());
 
-console.log(correctName("MohaMMad arMan"));
+// console.log(correctName("MohaMMad arMan"));
 
-// passanger name correction
-const passanger = "armAn MohaMMad"; // Arman Mohammad
-const lowerPassanger = passanger.toLowerCase(); // arman mohammad
+// // passanger name correction
+// const passanger = "armAn MohaMMad"; // Arman Mohammad
+// const lowerPassanger = passanger.toLowerCase(); // arman mohammad
 
-// re-read this code again to understand how its working as this logic is pretty sick
-const correctPassanger =
-  lowerPassanger[0].toUpperCase() +
-  lowerPassanger.slice(1, lowerPassanger.indexOf(" ")) +
-  " " +
-  lowerPassanger[lowerPassanger.indexOf(" ") + 1].toUpperCase() +
-  lowerPassanger.slice(lowerPassanger.indexOf(" ") + 2);
+// // re-read this code again to understand how its working as this logic is pretty sick
+// const correctPassanger =
+//   lowerPassanger[0].toUpperCase() +
+//   lowerPassanger.slice(1, lowerPassanger.indexOf(" ")) +
+//   " " +
+//   lowerPassanger[lowerPassanger.indexOf(" ") + 1].toUpperCase() +
+//   lowerPassanger.slice(lowerPassanger.indexOf(" ") + 2);
 
-console.log(correctPassanger);
-//--------------------------------------------------name right formatting end-------------------------------------
+// console.log(correctPassanger);
+// //--------------------------------------------------name right formatting end-------------------------------------
 
-//---------------------------------------emial comparison start------------------------------
-const email = "thisisdummyemail@dummy.com";
-const loginEmail = "   ThisISDUMMyEmail@DUmmy.com \n";
+// //---------------------------------------emial comparison start------------------------------
+// const email = "thisisdummyemail@dummy.com";
+// const loginEmail = "   ThisISDUMMyEmail@DUmmy.com \n";
 
-// const lowerEmail = loginEmail.toLowerCase();
-// const trimmedEmail = lowerEmail.trim();
-// const finalEmail = trimmedEmail;
-// console.log(finalEmail);
-// console.log(finalEmail === email);
+// // const lowerEmail = loginEmail.toLowerCase();
+// // const trimmedEmail = lowerEmail.trim();
+// // const finalEmail = trimmedEmail;
+// // console.log(finalEmail);
+// // console.log(finalEmail === email);
 
-//refactoring the above lines of code
-const normalise_email = loginEmail.toLowerCase().trim();
-console.log(normalise_email);
-//converting it in a function
-const emailCorrector = (user_email) => {
-  return user_email.toLowerCase().trim();
-};
+// //refactoring the above lines of code
+// const normalise_email = loginEmail.toLowerCase().trim();
+// console.log(normalise_email);
+// //converting it in a function
+// const emailCorrector = (user_email) => {
+//   const actual_email = user_email.toLowerCase().trim();
+//   if (actual_email === email) {
+//     return actual_email;
+//   } else {
+//     return "email does not match";
+//   }
+// };
 
-console.log(emailCorrector(loginEmail));
+// console.log(emailCorrector(loginEmail));
 //---------------------------------------emial comparison end------------------------------
 // console.log(plane[3]);
 
