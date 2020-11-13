b7c80f600e0bd340f6e26b78bdcb31185b67e3a2
aru99 <mohammadarmanmavric@gmail.com>
2020-11-13 16:52:39 +0530

    started working with strings module

---
+++ dataStructures/working_with_strings.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const arline = "luftanza";
+const plane = "A320";
+
+console.log(plane[3]);
+
+console.log(plane.length);
+
+// for (const [index, element] of plane) {
+//   console.log(element, index);
+// }
